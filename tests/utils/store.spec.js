7b35504b411530c5617f6c4e0704e06c612c2bc7
mcekiera <marcin.cekiera@gmail.com>
2020-01-29 17:05:44 +0100

    feat: create Store class

---
+++ tests/utils/store.spec.js
@@ -0,0 +1,34 @@
+const Store = require('./../../lib/utils/store');
+
+describe('Utils Store:', () => {
+  it('called with new should return class of Store object', () => {
+    const object = new Store();
+    expect(object).toBeInstanceOf(Store);
+  });
+
+  it('instance should not reveal data property', () => {
+    const object = new Store();
+    expect(object.data).toEqual(undefined);
+  });
+
+  it('get on non-existing property should throw error', () => {
+    const object = new Store();
+    expect(() => {
+      object.get('test');
+    }).toThrow(new Error('Requested property does not exists'));
+  });
+
+  it('set property should be retrived with get', () => {
+    const object = new Store();
+    object.set('test', 'test');
+    expect(object.get('test')).toEqual('test');
+  });
+
+  it('listProps should return list of props names', () => {
+    const object = new Store();
+    object.set('test', 'test');
+    expect(object.listProps()).toEqual(['test']);
+    object.set('test2', 'test2');
+    expect(object.listProps()).toEqual(['test', 'test2']);
+  });
+});
