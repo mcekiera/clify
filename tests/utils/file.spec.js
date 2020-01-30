302342f0d1556f5b3d66560aba02f3bf056d8dc8
mcekiera <marcin.cekiera@gmail.com>
2020-01-30 16:45:18 +0100

    fix: run eslint

--- tests/utils/file.spec.js
+++ tests/utils/file.spec.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
-const { save, load, listFiles } = require('./../../lib/utils/file');
 const fs = require('fs');
+const { save, load, listFiles } = require('./../../lib/utils/file');
 
 jest.mock('fs', () => ({
   existsSync: jest.fn(() => false),
   mkdirSync: jest.fn(),
   writeFileSync: jest.fn(),
-  readFile: jest.fn((name, cb) => cb(null, JSON.stringify({ test: 'test'}))),
+  readFile: jest.fn((name, cb) => cb(null, JSON.stringify({ test: 'test' }))),
   readdir: jest.fn((name, cb) => cb(null, ['test.json'])),
 }));
 
 describe('Utils file:', () => {
   it('should call fn on load call', async () => {
-    await save('name', { test: 'test'});
+    await save('name', { test: 'test' });
     expect(fs.existsSync).toBeCalled();
     expect(fs.mkdirSync).toBeCalled();
     expect(fs.writeFileSync).toBeCalled();
