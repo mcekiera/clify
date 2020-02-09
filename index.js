33738603a62f5e64290a5b8ccf72f23524d7ab6a
mcekiera <marcin.cekiera@gmail.com>
2020-02-09 20:04:06 +0100

    fix: make makeCli callable from outside lib

--- index.js
+++ index.js
@@ -4,38 +4,6 @@ const { prepareActions, prepareStructure } = require('./lib/utils/helpers');
 const Store = require('./lib/utils/Store');
 const Cli = require('./lib/Cli');
 
-const structureMock = {
-  api: {
-    get: (id) => ({
-      id,
-      props: true,
-      data: {
-        content: {
-          header: true,
-          title: 'title',
-        },
-      },
-    }),
-  },
-  utils: {
-    func1: (arg1, arg2) => {
-      console.info(`func1 ${arg1} ${arg2}`);
-    },
-    func2: (arg1, arg2) => {
-      console.info(`func2 ${arg1} ${arg2}`);
-    },
-  },
-};
-
-const store = new Store();
-const actions = prepareActions(store);
-prepareStructure(structureMock).then((structure) => {
-  const cli = new Cli(structure, {}, actions);
-  cli.run();
-}).catch((err) => {
-  console.error(err);
-});
-
 function makeCli(source) {
   const store = new Store();
   const actions = prepareActions(store);
